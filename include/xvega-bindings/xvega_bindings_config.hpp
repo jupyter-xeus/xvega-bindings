84553e9116ab3a70126d27ea9a92ed0e38348cdc
Johan Mabille <johan.mabille@gmail.com>
2024-06-06 17:31:06 +0200

    Refactored CMakeLists.txt and packaging logic

---
+++ include/xvega-bindings/xvega_bindings_config.hpp
@@ -0,0 +1,18 @@
+/***************************************************************************
+* Copyright (c) 2024, QuantStack and xeus-SQLite contributors              *
+*                                                                          *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XVEGA_BINDINGS_CONFIG_HPP
+#define XVEGA_BINDINGS_CONFIG_HPP
+
+#define XVEGA_BINDINGS_VERSION_MAJOR 0
+#define XVEGA_BINDINGS_VERSION_MINOR 0
+#define XVEGA_BINDINGS_VERSION_PATCH 10
+
+#endif
+
